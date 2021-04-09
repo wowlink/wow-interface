bd0c1e1cd74bef68ba3b98d0532b65df43f128fb
Tianhao Zhou <tianhaoz@umich.edu>
2021-04-09 02:59:26 +0000

    feat: add request and response interface

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,12 @@
-interface WowContext {
+interface WowMappingsFetchRequest {
     mappingSource: "github" | "gist" | "local" | "custom";
+    githubUser?: string;
+    githubRepository?: string;
+    gistUrl?: string;
+    localFilename?: string;
+    customApiEndpoint?: string;
+}
+
+interface WowMappingsFetchResponse {
     wowMapping: Record<string, string>;
 }
