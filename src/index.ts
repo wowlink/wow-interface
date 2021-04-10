8279d36aaa073df851e6c63da870d33cc8148c58
Tianhao Zhou <tianhaoz@umich.edu>
2021-04-10 05:57:40 +0000

    docs: add comments for future plans

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 export interface WowMappingsFetchRequest {
+    // For now the only allowed source is github. More source
+    // types will be added in the future to help with more use
+    // cases.
     mappingSource: "github" | "gist" | "local" | "custom";
     githubUser?: string;
     githubRepository?: string;
