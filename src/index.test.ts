65f7aa2310608cfa3d76ad668a17d23e6c1b9c40
Tianhao Zhou <tianhaoz@umich.edu>
2021-05-14 05:59:10 +0000

    feat: add data type for search ranking

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,17 +1,69 @@
-import { WowLookupFetchRequest, WowLookupFetchResponse } from ".";
+import {
+    WowLookupFetchRequest,
+    WowLookupFetchResponse,
+    WowUrlConverter,
+    WowUrlConverterConfig,
+    WowUrlConvertRequest,
+    WowUrlConvertResponse
+} from ".";
 
-test('Create request', () => {
-    const req: WowLookupFetchRequest = {
-        mappingSource: "github"
-    };
-    expect(req).toBeDefined();
+describe('Fetcher interface tests', () => {
+    test('Create request', () => {
+        const req: WowLookupFetchRequest = {
+            mappingSource: "github"
+        };
+        expect(req).toBeDefined();
+    });
+
+    test('Create response', () => {
+        const res: WowLookupFetchResponse = {
+            wowMapping: {
+                'gh': 'https://github.com'
+            }
+        };
+        expect(res).toBeDefined();
+    });
 });
 
-test('Create response', () => {
-    const res: WowLookupFetchResponse = {
-        wowMapping: {
-            'gh': 'https://github.com'
-        }
-    }
-    expect(res).toBeDefined();
+describe('Converter interface tests', () => {
+    test('Create request', () => {
+        const req: WowUrlConvertRequest = {
+            wowUrl: 'test_wow_url'
+        };
+        expect(req).toBeDefined();
+    });
+
+    test('Create response', () => {
+        // TODO(tianhaoz95): add another test to check
+        // it should fail without searchRanking once the
+        // field has become required.
+        const res: WowUrlConvertResponse = {
+            fullUrl: 'test_full_url',
+            searchRanking: []
+        };
+        expect(res).toBeDefined();
+    });
+
+    test('Create config', () => {
+        const config: WowUrlConverterConfig = {
+            fetcherResponse: {
+                wowMapping: {}
+            }
+        };
+        expect(config).toBeDefined();
+    });
+
+    test('Create converter', () => {
+        class DummyConverter implements WowUrlConverter {
+            convert(req: WowUrlConvertRequest): WowUrlConvertResponse {
+                const res: WowUrlConvertResponse = {
+                    fullUrl: 'test_full_url',
+                    searchRanking: []
+                };
+                return res;
+            }
+        };
+        const converter: WowUrlConverter = new DummyConverter();
+        expect(converter).toBeDefined();
+    });
 });
--- src/index.ts
+++ src/index.ts
@@ -31,8 +31,32 @@ export interface WowUrlConvertRequest {
     wowUrl: string;
 }
 
+/**
+ * This is used to pack ranking information
+ * together with the full URL itself which
+ * can be used by any client UI to show more
+ * information to help users decide which URL
+ * is a better match for the search query.
+ */
+export interface WowUrlRankingItem {
+    fullUrl: string;
+}
+
 export interface WowUrlConvertResponse {
     fullUrl: string;
+    /**
+     * This is used for ranking the potential
+     * matching URLs when there is no direct
+     * match.
+     * 
+     * For backward compatibility, it is marked
+     * as optional for now, but will become
+     * required when clients finish migration.
+     * 
+     * TODO(tianhaoz95): change this to required
+     * once dependent migration is done.
+     */
+    searchRanking?: WowUrlRankingItem[];
 }
 
 export interface WowUrlConverterConfig {
